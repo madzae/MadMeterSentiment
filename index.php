9881c467fb6e0b814ebf1932f0f2d4e4331557df
Ahmad Z <madzae@yahoo.com>
2024-05-22 17:22:40 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'languageMachine.php';
+
+use function Madzae\MadMeterSentiment;
+
+$sentences = 
+  "Liverpool menatap era baru seusai ditinggalkan Juergen Klopp. 
+  Manajer karismatik itu meninggalkan sejumlah catatan penting. 
+  Ia merupakan satu-satunya manajer sejauh ini yang menjuarai
+  Liga Inggris, Liga Champions, Piala FA, dan Piala Liga Inggris.";
+
+MadMeterSentiment($sentences);
+?> 
